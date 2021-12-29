c28788f3d4ce9ef1ccaf40c1860031c962ac6cd3
Nabinsharma123 <nabinsharma16.ns@gmail.com>
2021-12-29 12:17:43 +0530

    update 3

--- game.js
+++ game.js
@@ -5,29 +5,29 @@ var gamePattern = [];
 var userClickedPattern = [];
 var start = 0;
 var level = 0;
+var HighScore = 0;
 
 
-$(document).on("keypress",function () {
+$(document).on("keypress", function () {
     if (start === 0) {
-        nextSequence();   
-        setTimeout(function(){start = 1;},200);     
+        nextSequence();
+        setTimeout(function () { start = 1; }, 200);
     }
 
 });
 
 
-$(".btn").on("click",function () {
+$(".btn").on("click", function () {
 
     var userChosenColour = $(this).attr("id");
-    var color=$("#"+userChosenColour).css("background-color");
-    $(".footer").css("color",color);
-    
-    if(start===1)
-    {
-        playSound(userChosenColour);
-    userClickedPattern.push(userChosenColour);
+    var color = $("#" + userChosenColour).css("background-color");
+    $(".footer").css("color", color);
 
-    checkAnswer(userClickedPattern.length - 1);
+    if (start === 1) {
+        playSound(userChosenColour);
+        userClickedPattern.push(userChosenColour);
+
+        checkAnswer(userClickedPattern.length - 1);
     }
 
 });
@@ -35,9 +35,13 @@ $(".btn").on("click",function () {
 
 
 function nextSequence() {
-        userClickedPattern = [];
+    userClickedPattern = [];
 
     level++;
+    if (level > HighScore) {
+        HighScore = level;
+        $("#highscore").html("High Score : " + HighScore);
+    }
     $("#title").html("level " + level);
     var randomnumber = Math.floor(Math.random() * 4);
 
@@ -68,31 +72,29 @@ function checkAnswer(currentLevel) {
     if (userClickedPattern[currentLevel] === gamePattern[currentLevel]) {
 
         if (userClickedPattern.length === gamePattern.length) {
-            start=0;
-             setTimeout(function() {nextSequence();},500)   ;
-             setTimeout(function() {start=1;},500) ;
+            start = 0;
+            setTimeout(function () { nextSequence(); }, 500);
+            setTimeout(function () { start = 1; }, 500);
 
         }
     }
-    else
-       { 
-           startOver();
+    else {
+        startOver();
         playSound("wrong");
         $("body").addClass("game-over");
         setTimeout(function () {
             $("body").removeClass("game-over");
-          }, 200);
+        }, 200);
         $("#title").html("Game Over, Press Any Key to Restart");
-      
+
     }
 }
 
 
-function startOver()
-{
-    start=0;
-    level=0;
-    gamePattern=[];
+function startOver() {
+    start = 0;
+    level = 0;
+    gamePattern = [];
 
 }
 
